68ec38c63a6633b283bc4bcc934e73728e5970af
clementtyh <119471913+clementtyh@users.noreply.github.com>
2023-09-10 20:05:18 +0800

    Update host name

--- app/main.py
+++ app/main.py
@@ -24,4 +24,4 @@ async def custom_exception_handler(request, exc: HTTPException):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8081, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
