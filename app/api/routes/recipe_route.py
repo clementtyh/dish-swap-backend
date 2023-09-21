280979b26ba2879e01c9a6c6494b41c9295eca02
vxncetxn <thevancetan@gmail.com>
2023-09-21 16:31:21 +0800

    Added find one recipe route; Added recipe exceptions related to find one route

--- app/api/routes/recipe_route.py
+++ app/api/routes/recipe_route.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from models.response import ErrorOut
-from services.recipe_services import get_recipes
+from services.recipe_services import get_recipes, get_recipe
+from exceptions.recipe_exceptions import RecipeNotFoundException
 
 router = APIRouter()
 
@@ -9,12 +10,26 @@ async def root():
     return {"route": "recipe"}
 
 @router.get("/all/{page}")
-async def root(page):
+async def getAll(page):
     try:
         recipes = await get_recipes(page)
 
         return recipes
 
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=ErrorOut(message=str(e)).model_dump())
+    
+@router.get("/{id}")
+async def getOne(id):
+    try:
+        recipe = await get_recipe(id)
+
+        return recipe
+
+    except RecipeNotFoundException as e:
+        print(e)
+        raise HTTPException(status_code=404, detail=ErrorOut(message=str(e)).model_dump())
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail=ErrorOut(message=str(e)).model_dump())
--- app/services/recipe_services.py
+++ app/services/recipe_services.py
@@ -1,4 +1,6 @@
 from core.database import MongoDBConnector
+from bson.objectid import ObjectId
+from exceptions.recipe_exceptions import InvalidRecipeIDException, RecipeNotFoundException
 
 # Get singleton db connection
 recipe_db_collection = MongoDBConnector.get_client()["dishswapdb"]["recipes"]
@@ -11,6 +13,23 @@ async def get_recipes(page):
             recipe["_id"] = str(recipe["_id"])
 
         return recipes
+        
+    except Exception as e:
+        raise
+
+async def get_recipe(id):
+    try:
+        if not ObjectId.is_valid(id):
+            raise InvalidRecipeIDException(id)
+
+        recipe = await recipe_db_collection.find_one({"_id": ObjectId(id)})
+
+        if recipe is None:
+            raise RecipeNotFoundException(id)
+        
+        recipe["_id"] = str(recipe["_id"])
+
+        return recipe
         
     except Exception as e:
         raise
