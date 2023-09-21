7d7446b247d5cfb8552e1dfe2758970c70ada1a7
vxncetxn <thevancetan@gmail.com>
2023-09-21 15:32:34 +0800

    Added get all recipes route with pagination

--- app/api/routes/recipe_route.py
+++ app/api/routes/recipe_route.py
@@ -1,24 +1,19 @@
 from fastapi import APIRouter, HTTPException
-
-from models.response import SuccessOut, ErrorOut
-
+from models.response import ErrorOut
 from services.recipe_services import get_recipes
 
-
 router = APIRouter()
-
 
 @router.get("/")
 async def root():
     return {"route": "recipe"}
 
-
 @router.get("/all/{page}")
-async def root():
+async def root(page):
     try:
-        recipes = await get_recipes()
+        recipes = await get_recipes(page)
 
-        return SuccessOut()
+        return recipes
 
     except Exception as e:
         print(e)
--- app/services/recipe_services.py
+++ app/services/recipe_services.py
@@ -1,19 +1,15 @@
-from bson.objectid import ObjectId
-
-from models.user import UserDatabaseIn
-
 from core.database import MongoDBConnector
-
-from exceptions.user_exceptions import UserAlreadyExistsException, PasswordsDoNotMatchException
 
 # Get singleton db connection
 recipe_db_collection = MongoDBConnector.get_client()["dishswapdb"]["recipes"]
 
-
-async def get_recipes():
+async def get_recipes(page):
     try:
-        recipes = [recipe async for recipe in recipe_db_collection.find({}, skip=0, limit=1)]
-        print(recipes)
+        recipes = [recipe async for recipe in recipe_db_collection.find({}, skip=9*(int(page)-1), limit=9)]
+
+        for recipe in recipes:
+            recipe["_id"] = str(recipe["_id"])
+
         return recipes
         
     except Exception as e:
