2e4f1e52d5fe315f07c99a20443b0baa491c0207
vxncetxn <thevancetan@gmail.com>
2023-09-27 16:51:51 +0800

    Added search functionality to recipes root route

--- app/api/routes/recipe_route.py
+++ app/api/routes/recipe_route.py
@@ -7,9 +7,9 @@ from exceptions.recipe_exceptions import RecipeNotFoundException
 router = APIRouter()
 
 @router.get("/")
-async def root(page=1):
+async def root(page=1, search=""):
     try:
-        result = await get_recipes(page)
+        result = await get_recipes(page, search)
         headers = {"X-Total-Count": str(result["count"])}
 
         return JSONResponse(content=result["recipes"], headers=headers)
--- app/services/recipe_services.py
+++ app/services/recipe_services.py
@@ -1,3 +1,4 @@
+import re
 from core.database import MongoDBConnector
 from bson.objectid import ObjectId
 from exceptions.recipe_exceptions import InvalidRecipeIDException, RecipeNotFoundException
@@ -5,10 +6,20 @@ from exceptions.recipe_exceptions import InvalidRecipeIDException, RecipeNotFoun
 # Get singleton db connection
 recipe_db_collection = MongoDBConnector.get_client()["dishswapdb"]["recipes"]
 
-async def get_recipes(page):
+async def get_recipes(page, search):
     try:
-        count = await recipe_db_collection.count_documents({})
-        recipes = [recipe async for recipe in recipe_db_collection.find({}, skip=9*(int(page)-1), limit=9)]
+        search_regx = re.compile(fr'.*{re.escape(search)}.*', re.IGNORECASE)
+
+        count = await recipe_db_collection.count_documents(
+            {'$or': [{'name': {'$regex': search_regx}}, 
+                    {'description': {'$regex': search_regx}}]}
+        )
+        recipes = [recipe async for recipe in recipe_db_collection.find(
+            {'$or': [{'name': {'$regex': search_regx}}, 
+                    {'description': {'$regex': search_regx}}]}, 
+            skip=9*(int(page)-1), 
+            limit=9
+        )]
 
         for recipe in recipes:
             recipe["_id"] = str(recipe["_id"])
